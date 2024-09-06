71c34ff30ccb1e018cddb3500614610fd1a9679c
James Coglan <jcoglan@gmail.com>
2024-09-06 17:11:36 +0100

    Add Node and web implementations for AES-256-GCM

--- test/crypto_test.js
+++ test/crypto_test.js
@@ -34,6 +34,70 @@ function testCrypto (impl) {
         'yyfAwelMFCQzZ/+q/2aymE5bGH7H29urJMY3ui5Y9ig=')
     })
   })
+
+  describe('AES-256-GCM', () => {
+    it('generates a key', async () => {
+      let key = await impl.aes256gcm.generateKey()
+      assert(key instanceof Buffer)
+      assert.equal(key.length, 32)
+    })
+
+    it('generates an IV', async () => {
+      let iv = await impl.aes256gcm.generateIv()
+      assert(iv instanceof Buffer)
+      assert.equal(iv.length, 16)
+    })
+
+    it('encrypts a message', async () => {
+      let key = Buffer.from('jam1+7s+qyvQfaBZtIfS35/KSlt3QWlyr7OjsT6rp8E=', 'base64')
+      let iv = Buffer.from('vzI0dsZR/rdkmYflzpOJwA==', 'base64')
+      let msg = Buffer.from('the quick brown fox jumps over the lazy dog', 'utf8')
+
+      let data = await impl.aes256gcm.encrypt(key, iv, msg)
+
+      assert.equal(
+        data.toString('base64'),
+        'YJG5PBzwVW57j/w1N7lIDAN+w7TgsY5+GOzYuso6+Q0ptS8pwleLNDigJ4OEZ6A5tuseLNjX7FURi74=')
+    })
+
+    describe('decrypt()', () => {
+      let key = Buffer.from('hSZO6x/ffuPhW1aNmeSUB5vBV/ocTDtlbGeODN26Ovw=', 'base64')
+      let iv = Buffer.from('uvN7ZnNI6Ob3c/Of10K5tw==', 'base64')
+
+      it('decrypts a message', async () => {
+        let msg = Buffer.from('jqicVSHoa+ggSTOIv7KlCDVrsykiMX0+krljMx72HVAq11zh3RNG', 'base64')
+
+        let data = await impl.aes256gcm.decrypt(key, iv, msg)
+        assert.equal(data.toString('utf8'), 'very secret information')
+      })
+
+      it('fails to decrypt a modified ciphertext', async () => {
+        let msg = Buffer.from('kqicVSHoa+ggSTOIv7KlCDVrsykiMX0+krljMx72HVAq11zh3RNG', 'base64')
+        let error
+
+        try {
+          await impl.aes256gcm.decrypt(key, iv, msg)
+        } catch (e) {
+          error = e
+        }
+
+        assert(error)
+      })
+
+      it('fails to decrypt a modified auth tag', async () => {
+        let msg = Buffer.from('jqicVSHoa+ggSTOIv7KlCDVrsykiMX0+krljMx72HVAq11zh3RNX', 'base64')
+        let error
+
+        try {
+          await impl.aes256gcm.decrypt(key, iv, msg)
+        } catch (e) {
+          error = e
+        }
+
+        assert(error)
+      })
+    })
+  })
 }
 
 describe('crypto (node)', () => {
