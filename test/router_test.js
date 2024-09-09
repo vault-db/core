8443f5e8d67255b6f5db66f6f36bc236d0053b16
James Coglan <jcoglan@gmail.com>
2024-09-09 17:39:31 +0100

    Router.generateKey() should return a Buffer and not pre-encode the key

--- test/router_test.js
+++ test/router_test.js
@@ -5,7 +5,7 @@ const { assert } = require('chai')
 
 describe('Router', () => {
   it('returns shard IDs at level 0', async () => {
-    let key = 'dPKDVtT72E2hZd5shFi+iRCBCjBdXaP26EcsX+9PpgGG+WK8rGtU6ZPAz1AtL1B8tm88Wyp3I5dVflUMg0IY7Q=='
+    let key = Buffer.from('dPKDVtT72E2hZd5shFi+iRCBCjBdXaP26EcsX+9PpgGG+WK8rGtU6ZPAz1AtL1B8tm88Wyp3I5dVflUMg0IY7Q==', 'base64')
     let router = new Router({ key, level: 0 })
 
     for (let n of 'abcdefghijklmnopqrstuvwxyz'.split('')) {
@@ -14,7 +14,7 @@ describe('Router', () => {
   })
 
   it('returns shard IDs at level 1', async () => {
-    let key = 'frjOsNUsLE9VfIM7vtN5Yk/gdlYUpb8lZezpf9ES/NJTUYaeMuAysGGOvtzwVbv7ZdrSIbDDHYJi5+pDlDLsFA=='
+    let key = Buffer.from('frjOsNUsLE9VfIM7vtN5Yk/gdlYUpb8lZezpf9ES/NJTUYaeMuAysGGOvtzwVbv7ZdrSIbDDHYJi5+pDlDLsFA==', 'base64')
     let router = new Router({ key, level: 1 })
 
     assert.equal(await router.getShardId('/a'), 'shard-0')
@@ -24,7 +24,7 @@ describe('Router', () => {
   })
 
   it('returns shard IDs at level 2', async () => {
-    let key = 'yNIdoAbi9oJtFuxsWw4lWxa82bQMrOs4VYtXTIc+tiNGE2W4gnaqsdbCLedoj2VA4oUtb24wgkFTow8Pol2Hig=='
+    let key = Buffer.from('yNIdoAbi9oJtFuxsWw4lWxa82bQMrOs4VYtXTIc+tiNGE2W4gnaqsdbCLedoj2VA4oUtb24wgkFTow8Pol2Hig==', 'base64')
     let router = new Router({ key, level: 2 })
 
     assert.equal(await router.getShardId('/a'), 'shard-1')
@@ -34,7 +34,7 @@ describe('Router', () => {
   })
 
   it('returns shard IDs at level 3', async () => {
-    let key = 'r6dXMLaJhaPcBK7u98fNHSr2SUrP9QItuwr+eZFXFJvVgRnS9xdBV4h2FkZHG1/cm6Eweg6BnppC7ueeeMweEg=='
+    let key = Buffer.from('r6dXMLaJhaPcBK7u98fNHSr2SUrP9QItuwr+eZFXFJvVgRnS9xdBV4h2FkZHG1/cm6Eweg6BnppC7ueeeMweEg==', 'base64')
     let router = new Router({ key, level: 3 })
 
     assert.equal(await router.getShardId('/a'), 'shard-4')
@@ -48,7 +48,7 @@ describe('Router', () => {
   })
 
   it('returns shard IDs at level 5', async () => {
-    let key = 'v91VlCeE3rit6LGjhfW+Cc0Lv6kK0lwkg4cxZXDmDQ/VSCrvzd8pyZ4DpuOT6NGqxTOGQ7gc20NteLMbjWxGGA=='
+    let key = Buffer.from('v91VlCeE3rit6LGjhfW+Cc0Lv6kK0lwkg4cxZXDmDQ/VSCrvzd8pyZ4DpuOT6NGqxTOGQ7gc20NteLMbjWxGGA==', 'base64')
     let router = new Router({ key, level: 5 })
 
     assert.equal(await router.getShardId('/a'), 'shard-1d')
